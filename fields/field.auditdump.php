beda87f9c0b33d3e937e60d247c041e6cb16fa24
Rowan Lewis <rowan@devo.randb.com.au>
2010-01-15 08:19:20 +1000

    Fixed issue where array wasn't being displayed.

--- fields/field.auditdump.php
+++ fields/field.auditdump.php
@@ -76,7 +76,7 @@
 			$pre = new XMLElement('pre');
 			
 			$pre->appendChild(new XMLElement(
-				'code', General::sanitize(unserialize($data))
+				'code', General::sanitize(var_export(unserialize($data), true))
 			));
 			
 			$label->appendChild($pre);
@@ -126,7 +126,7 @@
 		public function appendFormattedElement(&$wrapper, $data, $encode = false, $mode = null) {
 			$wrapper->appendChild(new XMLElement(
 				$this->get('element_name'),
-				General::sanitize(unserialize($value))
+				General::sanitize(var_export(unserialize($data), true))
 			));
 		}
 	}
